9e229ab58abad690729d76a72d0dd40b1ecda329
zz990099 <771647586@qq.com>
2025-05-02 13:47:15 +0800

    Update model convertion script on jetson
    Signed-off-by: zz990099 <771647586@qq.com>

--- tools/cvt_onnx2trt.sh
+++ tools/cvt_onnx2trt.sh
@@ -12,8 +12,8 @@
                               --saveEngine=/workspace/models/memory_encoder.engine \
                               --fp16
 
-/usr/src/tensorrt/bin/trtexec --onnx=/workspace/models/memory_attention.onnx \
-                              --saveEngine=/workspace/models/memory_attention.engine \
+/usr/src/tensorrt/bin/trtexec --onnx=/workspace/models/memory_attention_opt.onnx \
+                              --saveEngine=/workspace/models/memory_attention_opt.engine \
                               --minShapes=memory_0:1x256,memory_1:1x64x64x64,memory_pos_embed:4100x1x64 \
                               --optShapes=memory_0:16x256,memory_1:7x64x64x64,memory_pos_embed:28736x1x64 \
                               --maxShapes=memory_0:16x256,memory_1:7x64x64x64,memory_pos_embed:28736x1x64 \
